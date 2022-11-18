c2f8f3ee22c2c48cba0762aeb39f2c7f837dbb0c
Luhenrique09 <luhenrique1100@gmail.com>
2022-11-18 18:28:21 -0300

    feat: checking user already exists in middlewares

--- src/middlewares/validate.users.middlewares.js
+++ src/middlewares/validate.users.middlewares.js
@@ -1,12 +1,20 @@
 import {userSchema} from "../index.js"
-
-export function validateUsersSchema (req, res, next){
+import { db } from "../database/db.js";
+export async function validateUsersSchema (req, res, next){
     const user = req.body;
     const { error } = userSchema.validate(user, { abortEarly: false })
 
         if (error) {
             const errors = error.details.map((detail) => detail.message)
             return res.status(400).send(errors)
+        }
+        try {
+            const userExists = await db.collection("users").findOne({ email: user.email })
+            if (userExists) {
+                return res.status(409).send({ message: "Esse email já existe" })
+            }
+        } catch (err) {
+            return res.sendStatus(500)
         }
         next();
 }
